8292db1f0af41c8ebc21a290fe39184a68925e5f
Ankit <ankitlad1999@gmail.com>
2023-09-08 20:29:53 +0530

    Hello and var file added

---
+++ var-let-const.js
@@ -0,0 +1,17 @@
+
+// "let" -->this use more in today for declare variable
+// the "var"--> even if declare varible name same in more type it not show error. not to prefer use this
+// "const"  --> this is use to make our variable unchange or we also can't change or update value of this variable
+
+
+let a = 5; //let not allow to declare more variable of same name
+console.log("value of a is: " + a);
+
+var b = 6;
+console.log("the value of b is : "+ b);  // var variable we can declare in many times
+
+var b = "Ankit";
+console.log("value of B is : " + b);
+
+const c= true;
+console.log(c);
